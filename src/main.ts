f4a6c100af59cd572070878c293ee28b2d127bcc
jos3lo89 <gothangel21@hotmail.com>
2025-01-24 10:58:36 -0500

    change prod

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    // origin: 'http://localhost:5173',
+    origin:
+      process.env.DEV !== 'development'
+        ? process.env.CLIENT_URL
+        : 'http://localhost:5173',
     credentials: true,
   });
 
